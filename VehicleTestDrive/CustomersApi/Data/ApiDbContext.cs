cf16bf960cf94ab2d75e76076ac59d453ec3fef0
Shrikant Thombre <shri1900@gmail.com>
2022-04-12 13:06:53 +0100

    Added Customer API with data migrations

---
+++ VehicleTestDrive/CustomersApi/Data/ApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CustomersApi.Models;
+
+namespace CustomersApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=CustomerApiDb;");
+            optionsBuilder.UseSqlServer(@"Data Source=SHRIKANT-PC\MSSQLSERVER01;Initial Catalog=CustomerApiDb;Integrated Security=True");
+        }
+    }
+}
